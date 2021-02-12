e8abb8c27d95eee41a24e9f8953957098bc700fe
Liam <ljcallaghan64@gmail.com>
2021-02-12 23:10:58 +0000

    movie card in the works

--- src/containers/movies/Movies.tsx
+++ src/containers/movies/Movies.tsx
@@ -1,27 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { MovieCard } from '../../components/Components';
 import { movie } from './Movie.model';
 
 export const Movies = () => {
-    
-    const movie1: movie = {
-        title: 'hello',
-        length: 1234,
-        releaseDate: 'some year'
-    }
-    
-    const [movie, setMovie] = useState({
-        title: 'default',
-        length: 0,
-        releaseDate: 'default'
-    })
+
+  const movie1: movie = {
+    title: 'hello',
+    length: 1234,
+    releaseDate: 'some year'
+}
+const movie2: movie = {
+  title: 'hello',
+  length: 12345,
+  releaseDate: 'some year'
+}
+const movie3: movie = {
+  title: 'hello',
+  length: 123456,
+  releaseDate: 'some year'
+}
+const movie4: movie = {
+  title: 'hello',
+  length: 1234567,
+  releaseDate: 'some year'
+}
+
+const arr = [movie1, movie2, movie3, movie4]
 
     return (
         <div>
-            <h1>Movie</h1>
-            <div>{movie.title}</div>
-            <div>{movie.length}</div>
-            <div>{movie.releaseDate}</div>
-            <button onClick={() => setMovie(movie1)}>Update state</button>
+          <div className="container my-12 mx-auto px-4 md:px-12">
+            <div className="flex flex-wrap -mx-1 lg:-mx-4">
+              {this.arr.map(movie => (<MovieCard key={movie.length} {...movie} />))}
+            </div>
+          </div>
         </div>
     )
 }
